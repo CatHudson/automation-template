669fcdbdcd6eafeb161273e7404455f96649a88d
CatHudson <72917346+CatHudson@users.noreply.github.com>
2025-02-14 15:06:48 +0400

    add CRUD and Roles test cases

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,12 +13,14 @@ dependencies {
     val junitVersion = "5.11.4"
     val seleniumVersion = "4.28.1"
     val restAssuredVersion = "5.5.0"
+    val allureVersion = "2.29.1"
 
     implementation("org.seleniumhq.selenium:selenium-java:$seleniumVersion")
     implementation("io.rest-assured:rest-assured:$restAssuredVersion")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    testImplementation("io.qameta.allure:allure-junit5:$allureVersion")
 }
 
 tasks.test {
---
+++ src/test/kotlin/com/jetbrains/teamcity/api/BuildTypeTest.kt
@@ -0,0 +1,57 @@
+package com.jetbrains.teamcity.api
+
+import io.qameta.allure.Allure.step
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+
+@Tag("Regression")
+class BuildTypeTest: BaseApiTest() {
+
+    @Test
+    @Tag("CRUD")
+    @Tag("Positive")
+    fun `user creates a buildType`() {
+        step("Create a user")
+        step("Create a project by the user")
+        step("Create a build type for the project by the user")
+        step("Assert that the build type was created successfully with correct data")
+    }
+
+    @Test
+    @Tag("CRUD")
+    @Tag("Negative")
+    fun `user cannot create the second buildType with already existing id`() {
+        step("Create a user")
+        step("Create a project by the user")
+        step("Create a build type for the project by the user")
+        step("Create the second build type for the project by the user")
+        step("Assert that the second build type was not created due to already existing id")
+    }
+
+    @Test
+    @Tag("Roles")
+    @Tag("Positive")
+    fun `project-admin creates a buildType`() {
+        step("Create a user")
+        step("Grant the user PROJECT_ADMIN privileges")
+        step("Create a project by the project-admin")
+        step("Create a build type for the project by the project-admin")
+        step("Assert that the build type was created successfully")
+    }
+
+    @Test
+    @Tag("Roles")
+    @Tag("Negative")
+    fun `project-admin cannot create a buildType for another project`() {
+        step("Create a user")
+        step("Grant the user PROJECT_ADMIN privileges")
+        step("Create a project by the project-admin")
+
+        step("Create the second user")
+        step("Grant the second user PROJECT_ADMIN privileges")
+        step("Create a project by the second project-admin")
+
+        step("Create a build type for the second project by the first project-admin")
+        step("Assert that the build type was not created with FORBIDDEN code")
+    }
+}
