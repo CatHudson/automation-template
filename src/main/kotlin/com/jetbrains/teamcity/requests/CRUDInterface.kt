fe5cac43fb0fd06551a6beede89bbb2e41aeb219
cathudson <arturhudson85@gmail.com>
2025-02-18 11:57:19 +0400

    add helpers

--- src/main/kotlin/com/jetbrains/teamcity/requests/CRUDInterface.kt
+++ src/main/kotlin/com/jetbrains/teamcity/requests/CRUDInterface.kt
@@ -1,10 +1,11 @@
 package com.jetbrains.teamcity.requests
 
+import com.jetbrains.teamcity.enums.ReadQueryIdType
 import com.jetbrains.teamcity.models.BaseModel
 
 interface CRUDInterface {
     fun create(model: BaseModel): Any
-    fun read(id: String): Any
+    fun read(value: String, param: ReadQueryIdType = ReadQueryIdType.ID): Any
     fun update(id: String, model: BaseModel): Any
     fun delete(id: String): Any
 }
