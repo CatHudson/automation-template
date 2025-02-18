87309f1576973ae7a4d38e3a1cd3b02ae735b46e
cathudson <arturhudson85@gmail.com>
2025-02-18 14:17:49 +0400

    add helpers

--- src/main/kotlin/com/jetbrains/teamcity/requests/CRUDInterface.kt
+++ src/main/kotlin/com/jetbrains/teamcity/requests/CRUDInterface.kt
@@ -5,6 +5,9 @@ import com.jetbrains.teamcity.models.BaseModel
 
 interface CRUDInterface {
     fun create(model: BaseModel): Any
+    /**
+     * You can pass a parameter name to search by something other than id
+     */
     fun read(value: String, param: ReadQueryIdType = ReadQueryIdType.ID): Any
     fun update(id: String, model: BaseModel): Any
     fun delete(id: String): Any
