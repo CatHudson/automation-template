34a85bc21efac63509b8dfb304bf56ad253988e5
cathudson <arturhudson85@gmail.com>
2025-02-25 16:08:17 +0400

    save progress

--- src/main/kotlin/com/jetbrains/teamcity/ui/elements/ProjectElement.kt
+++ src/main/kotlin/com/jetbrains/teamcity/ui/elements/ProjectElement.kt
@@ -3,5 +3,5 @@ package com.jetbrains.teamcity.ui.elements
 import com.codeborne.selenide.SelenideElement
 
 class ProjectElement(element: SelenideElement) : BasePageElement(element) {
-    val name: SelenideElement = find("span[class*='MiddleEllipsis']")
+    val name = find("span[class*='MiddleEllipsis']")
 }
--- src/main/kotlin/com/jetbrains/teamcity/ui/pages/BuildTypePage.kt
+++ src/main/kotlin/com/jetbrains/teamcity/ui/pages/BuildTypePage.kt
@@ -1,21 +1,48 @@
 package com.jetbrains.teamcity.ui.pages
 
+import com.codeborne.selenide.Condition.visible
 import com.codeborne.selenide.Selenide
 import com.codeborne.selenide.Selenide.`$`
+import com.jetbrains.teamcity.ui.elements.BuildRunHistoryElement
 
 class BuildTypePage : BasePage() {
+
     private val buildRunHistoryBlock = `$`("div[class*='Builds__hasParentGrid']")
     private val buildRunHistoryElements = buildRunHistoryBlock.findAll("div[class*='buildContainer']")
     val buildsInQueueIndicator = buildRunHistoryBlock.find("button[class*='Builds'] span[class='ring-button-content']")
     val title = `$`("h1")
 
+    fun getBuildRuns(): List<BuildRunHistoryElement> {
+        buildRunHistoryBlock.shouldBe(visible)
+        return generatePageElements(buildRunHistoryElements) {
+            BuildRunHistoryElement(it)
+        }
+    }
+
+    init {
+        title.shouldBe(visible)
+    }
+
     companion object {
 
-        private const val PROJECT_URL = "/buildConfiguration/%s_%s"
+        private const val BUILD_TYPE_BY_NAME_URL = "/buildConfiguration/%s_%s"
+        private const val BUILD_TYPE_BY_ID_URL = "/buildConfiguration/%s"
 
         fun open(projectId: String, buildTypeName: String): BuildTypePage {
             return Selenide.open(
-                PROJECT_URL.format(projectId, buildTypeName.replace("_", "")),
+                BUILD_TYPE_BY_NAME_URL.format(
+                    projectId,
+                    buildTypeName.replace("_", "").let {
+                        it.replaceFirstChar { char -> char.uppercaseChar() }
+                    }
+                ),
+                BuildTypePage::class.java
+            )
+        }
+
+        fun open(buildTypeId: String): BuildTypePage {
+            return Selenide.open(
+                BUILD_TYPE_BY_ID_URL.format(buildTypeId),
                 BuildTypePage::class.java
             )
         }
