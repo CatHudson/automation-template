6e0d676ec6fdd1de120e87afbd6620919f90de92
cathudson <arturhudson85@gmail.com>
2025-02-25 16:56:32 +0400

    small fix

--- src/main/kotlin/com/jetbrains/teamcity/ui/pages/BuildTypePage.kt
+++ src/main/kotlin/com/jetbrains/teamcity/ui/pages/BuildTypePage.kt
@@ -9,7 +9,7 @@ class BuildTypePage : BasePage() {
 
     private val buildRunHistoryBlock = `$`("div[class*='Builds__hasParentGrid']")
     private val buildRunHistoryElements = buildRunHistoryBlock.findAll("div[class*='buildContainer']")
-    val buildsInQueueIndicator = buildRunHistoryBlock.find("div[class*='ring-button-content']")
+    val buildsInQueueIndicator = buildRunHistoryBlock.find("button[class*='Builds'] span[class='ring-button-content']")
     val title = `$`("h1")
 
     fun getBuildRuns(): List<BuildRunHistoryElement> {
